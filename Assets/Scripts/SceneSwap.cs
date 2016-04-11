b4f841111ff119e0bc1aacbab0ce1f07fb8019e2
Natalie Nygaard <natalie42@me.com>
2016-04-11 11:17:28 -0700

    added title scene

---
+++ Assets/Scripts/SceneSwap.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwap : MonoBehaviour {
+
+	public void ToGame() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+	}
+
+	public void ToMenu() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+	}
+}
